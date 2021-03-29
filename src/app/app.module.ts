6ae742a122f9acd94cbd2c644b13acb9162de37a
Felipe Simonette <felipe.house10@gmail.com>
2021-03-28 21:51:33 -0300

    Alterações visuais componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
+
   imports: [
-    BrowserModule
+    TooltipModule.forRoot(),
+    BrowserModule,
+    TooltipModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
