b668feafa592606f49cf6d9a5e05971571223746
Evandro <evandro.casarotte@gmail.com>
2021-03-29 22:17:42 -0300

    Tela de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public formLogin: FormGroup = new FormGroup({
+    email: new FormControl(null, Validators.required),
+    senha: new FormControl(null, Validators.required),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  autenticar(): void {
+    console.log('teste');
   }
 
 }
