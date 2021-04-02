c0fbe2ef235fc51e80c3ff07d5820d7a45d23700
Felipe Simonette <felipe.house10@gmail.com>
2021-04-01 23:08:10 -0300

    Criação do arquivo de rotas e validação de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponents } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent
+    routingComponents
   ],
 
   imports: [
     TooltipModule.forRoot(),
     BrowserModule,
     TooltipModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -8,18 +11,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  private readonly emailBase: string = "admin@admin.com";
+  private readonly senhaBase: string = "admin";
+
+  @ViewChild ('email') emailInput: ElementRef;
+  @ViewChild ('senha') senhaInput: ElementRef;
+
+
   public formLogin: FormGroup = new FormGroup({
-    email: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
     senha: new FormControl(null, Validators.required),
   });
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   autenticar(): void {
-    console.log('teste');
-  }
 
+    if (this.formLogin.invalid){
+      alert("Email ou Senha inválido");
+
+      // validação para email
+      // if (this.formLogin.errors == Validators.email){
+
+      // }
+
+    }
+    else {
+      if (this.emailInput.nativeElement.value === this.emailBase && this.senhaInput.nativeElement.value === this.senhaBase){
+        alert("Login efetuado com sucesso!");
+        this.router.navigate(['/home']);
+
+      }
+      else {
+        alert("Email ou Senha inválido");
+      }
+    }
+  }
 }
